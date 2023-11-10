07d38ac98ad80f09a1ff30e1202014c91543221c
C_elare <houvillev@gmail.com>
2023-11-10 20:39:58 +0100

    feat(factory): Add 'user_id' field to CategoryFactory
    - Add 'use App\Models\User' statement
    - Add 'user_id' field to factory definition
    - Set 'user_id' value to User::factory()

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -21,6 +22,7 @@ class CategoryFactory extends Factory
         $createdAt = fake()->dateTimeBetween('-2 years');
         return [
             'name' => fake()->sentence(1),
+            'user_id' => User::factory(),
             'created_at' => $createdAt,
             'updated_at'=>fake()->dateTimeBetween($createdAt, 'now'),
         ];
