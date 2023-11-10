38290757c7f8d2d30ff917f65b474620546fdad0
C_elare <houvillev@gmail.com>
2023-11-10 15:57:38 +0100

    feat(Article): Add Article model with relationships

---
+++ app/Models/Article.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    /**
+     * Get the user that authored the article.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the category of the article.
+     *
+     * @return BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * The tags associated with the article.
+     *
+     * This is a many-to-many relationship and uses a pivot table to maintain
+     * the relationship between the articles and tags.
+     *
+     * @return BelongsToMany
+     */
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'article_tags', 'article_id', 'tag_id');
+    }
+
+}
