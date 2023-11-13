47abbcafa45f8977297dc4d4e0156612a77b8d16
C_elare <houvillev@gmail.com>
2023-11-13 23:27:53 +0100

    feat(api): Add routes for categories, articles, and tags (main)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,11 +12,30 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Public Access for all users
+Route::apiResource('categories', CategoryController::class)
+    ->only(['index', 'show']);
+Route::apiResource('categories.articles', ArticleController::class)
+    ->scoped(['articles' => 'categories'])->only(['index', 'show']);
+Route::apiResource('tags', TagController::class)
+    ->only(['index', 'show']);
+
+// Protected routes for content management (store, update, destroy)
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('categories', CategoryController::class)
+        ->except(['index', 'show']);
+    Route::apiResource('categories.articles', ArticleController::class)
+        ->except(['index', 'show']);
+    Route::apiResource('tags', TagController::class)
+        ->except(['index', 'show']);
+});
+
