837c8c795d9d53bec33aa12bb0eeb7773792d21b
C_elare <houvillev@gmail.com>
2023-11-13 23:30:06 +0100

    feat: Add CategorySeeder.php (main)
    Add CategorySeeder.php with the ability to seed categories for each user.

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\User;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::all();
+
+        foreach ($users as $user) {
+            Category::factory()->count(3)->create(['user_id' => $user->id]);
+        }
+    }
+}
