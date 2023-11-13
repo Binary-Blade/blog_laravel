e93b582a45d47157ddc796c32f8d87f908593ab2
C_elare <houvillev@gmail.com>
2023-11-13 23:29:30 +0100

    feat(routes): Update web.php (main)

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,13 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::view('/', 'livewire.welcome.navigation');
-
-Route::view('navigation', 'livewire.layout.navigation');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+Route::get('/', function () {
+    return ['Laravel' => app()->version(), 'Coucou'];
+});
 
 require __DIR__.'/auth.php';
