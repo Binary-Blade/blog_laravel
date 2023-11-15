7823a7d732346a382c357bca0947a47bdb7707f3
C_elare <houvillev@gmail.com>
2023-11-15 02:23:24 +0100

    feat(api): Implement CRUD operations for ArticleController (main)
    - Added imports for Article and Category models
    - Added return type hint for index() method
    - Implemented index() method to return all articles
    - Implemented store() method to create a new article
    - Added validation for title, full_text, and image_path in store() method
    - Set user_id and category_id values in store() method
    - Implemented show() method to return a specific article
    - Implemented update() method to update an article
    - Added validation for title, full_text, and image_path in update() method
    - Implemented destroy() method to delete an article
    - Returned a 204 status code in destroy() method

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -10,9 +14,9 @@ class ArticleController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Collection
     {
-        //
+        return Article::all();
     }
 
     /**
@@ -20,30 +24,46 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            "title" => 'required|string|max:50',
+            "full_text" => 'nullable|string',
+            "image_path" => 'nullable|image',
+        ]);
+
+        $validatedData['user_id'] = 1;
+        $validatedData['category_id'] = $request->route('category');
+
+        return Article::create($validatedData);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Category $category, Article $article): Article
     {
-        //
+        return $article;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Category $category, Article $article): Article
     {
-        //
+        $article->update($request->validate([
+            "title" => 'sometimes|string|max:50',
+            "full_text" => 'nullable|string',
+            "image_path" => 'nullable|image',
+        ]));
+        return $article;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category, Article $article)
     {
-        //
+        $article->delete();
+
+        return response(status: 204);
     }
 }
