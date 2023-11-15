6ba9529ded1f3281a999f29c8353a7a798bf7ad9
C_elare <houvillev@gmail.com>
2023-11-15 02:24:35 +0100

    feat(api): Add TagController methods for CRUD operations (main)

--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
@@ -10,9 +12,9 @@ class TagController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Collection
     {
-        //
+        return Tag::all();
     }
 
     /**
@@ -20,30 +22,41 @@ class TagController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validateData = $request->validate([
+            "name" => "required|string|max:100"
+        ]);
+        $validateData['user_id'] = 1;
+
+        return Tag::create($validateData);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Tag $tag): Tag
     {
-        //
+        return $tag;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Tag $tag): Tag
     {
-        //
+        $tag->update($request->validate([
+            "name" => "sometimes|string|max:100"
+        ]));
+        return $tag;
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Tag $tag)
     {
-        //
+        $tag->delete();
+
+        return response(status: 204);
     }
 }
