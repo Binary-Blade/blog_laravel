57540da5c2463b1ecd823d40b00a4a0281c0867e
C_elare <houvillev@gmail.com>
2023-11-15 02:23:39 +0100

    feat(api): Add CategoryController and implement CRUD operations (#main)

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -10,9 +13,9 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Collection
     {
-        //
+       return Category::all();
     }
 
     /**
@@ -20,30 +23,41 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            "name"=> "required|string|max:50",
+        ]);
+
+        $validatedData['user_id'] = 1;
+
+        return Category::create($validatedData);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show( Category $category): Category
     {
-        //
+        return $category;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Category $category): Category
     {
-        //
+        $category->update($request->validate([
+            "name"=> "sometimes|string|max:50",
+        ]));
+        return $category;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return response(status: 204);
     }
 }
