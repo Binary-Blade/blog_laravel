bbb11b189909836dc910d7e9372e5765d67b6db8
C_elare <houvillev@gmail.com>
2023-11-15 22:09:52 +0100

    feat(api): Implement CategoryController methods (main) Changes: - Added the CanLoadRelationships trait to the CategoryController. - Added the $relations property to specify the relationships to load. - Refactored the index method to use the loadRelationships method. - Refactored the store method to validate and retrieve the request data. - Refactored the store method to create and return the new category. - Refactored the show method to load related articles for the category. - Refactored the update method to validate the request data and update the category. - Refactored the destroy method to delete the specified category.

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,85 +4,98 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryResource;
-use App\Models\Article;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Category;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 
+/**
+ * Class CategoryController
+ *
+ * Handles HTTP requests related to Category resources.
+ */
 class CategoryController extends Controller
 {
+    use CanLoadRelationships;
+
+    protected array $relations = ['user', 'articles', 'articles.user', 'articles.tags'];
     /**
      * Display a listing of the resource.
+     *
+     * @return AnonymousResourceCollection Returns a collection of Category resources.
      */
     public function index(): AnonymousResourceCollection
     {
-        $query = Category::query();
-        $relations = ['user', 'articles','articles.user'];
+        // Initialize the query builder for Category model
+        $query = $this->loadRelationships(Category::query());
 
-        foreach ($relations as $relation){
-            $query->when(
-                $this->shouldIncludeRelation($relation),
-                fn($queries) => $queries->with($relation)
-            );
-        }
-
-       return CategoryResource::collection(
-           $query->latest()->paginate());
-    }
-
-    protected function shouldIncludeRelation (string $relation): bool
-    {
-        $include =  \request()->query('include');
-        if(!$include) {
-            return false;
-        }
-        $relations = array_map('trim',explode(',', $include));
-
-        return in_array($relation, $relations);
+        // Return paginated and sorted collection of categories
+        return CategoryResource::collection(
+            $query->latest()->paginate()
+        );
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param Request $request The request object containing the data.
+     * @return CategoryResource The newly created Category object.
      */
-    public function store(Request $request)
+    public function store(Request $request): CategoryResource
     {
-        $validatedData = $request->validate([
-            "name"=> "required|string|max:50",
+        // Validate and retrieve the request data
+        $category = Category::create([
+            $request->validate([
+            "name" => "required|string|max:50",
+        ]),
+            'user_id' => 1,
         ]);
 
-        $validatedData['user_id'] = 1;
-
-        return Category::create($validatedData);
+        // Create and return the new category
+        return new CategoryResource($this->loadRelationships($category));
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param Category $category The category to display.
+     * @return CategoryResource Returns the specified Category resource.
      */
-    public function show( Category $category): CategoryResource
+    public function show(Category $category): CategoryResource
     {
-        $category->load('articles');
-        return new CategoryResource($category);
+        // Load related articles for the category
+        return new CategoryResource($this->loadRelationships($category));
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param Request $request The request object containing the data.
+     * @param Category $category The category to update.
+     * @return CategoryResource Returns the updated Category resource.
      */
     public function update(Request $request, Category $category): CategoryResource
     {
+        // Validate the request data and update the category
         $category->update($request->validate([
-            "name"=> "sometimes|string|max:50",
+            "name" => "sometimes|string|max:50",
         ]));
-        return new CategoryResource($category);
+        return new CategoryResource($this->loadRelationships($category));
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param Category $category The category to delete.
+     * @return Response Returns an HTTP response with status code.
      */
-    public function destroy(Category $category)
+    public function destroy(Category $category): Response
     {
+        // Delete the specified category
         $category->delete();
 
+        // Return a 204 No Content response
         return response(status: 204);
     }
 }
