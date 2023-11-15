2364d1a3b07a76a8ecd9a4566a1697b7fb8e8b06
C_elare <houvillev@gmail.com>
2023-11-15 03:08:15 +0100

    feat(api): Update CategoryController.php (main)
    - Update CategoryController.php to return CategoryResource instead of Collection
    - Update show() method to return CategoryResource instead of Category
    - Update update() method to return CategoryResource instead of Category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,19 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
 use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Collection
+    public function index(): AnonymousResourceCollection
     {
-       return Category::all();
+       return CategoryResource::collection(Category::all());
     }
 
     /**
@@ -35,20 +37,20 @@ class CategoryController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show( Category $category): Category
+    public function show( Category $category): CategoryResource
     {
-        return $category;
+        return new CategoryResource($category);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category): Category
+    public function update(Request $request, Category $category): CategoryResource
     {
         $category->update($request->validate([
             "name"=> "sometimes|string|max:50",
         ]));
-        return $category;
+        return new CategoryResource($category);
     }
 
     /**
