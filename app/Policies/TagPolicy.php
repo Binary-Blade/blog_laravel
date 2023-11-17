6da3a3665b143277d7de006a7275b732588adaf5
C_elare <houvillev@gmail.com>
2023-11-17 02:15:27 +0100

    feat(TagPolicy): add can method to check if user can view a tag (main)

--- app/Policies/TagPolicy.php
+++ app/Policies/TagPolicy.php
@@ -48,19 +48,4 @@ class TagPolicy
         return $user->id === $tag->user_id;
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Tag $tag): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Tag $tag): bool
-    {
-        //
-    }
 }
